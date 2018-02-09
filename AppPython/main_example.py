58ea00ab49c2b5be9a4860a36910967249d12b6b
Your Name <patirck.heenan@colorado.edu>
2018-02-09 13:48:15 -0700

    added in python run script for unfold an refold (which looks OK...), but just unfold or just refold dont.

--- AppPython/main_example.py
+++ AppPython/main_example.py
@@ -12,6 +12,21 @@ import sys,os
 sys.path.append("../")
 from UtilGeneral import PlotUtilities
 
+def run_single(n_pairs,v,input_file,extra_str=""):
+    run_str = (r"python2 main_iwt.py -number_of_pairs {}" + \
+               r" -number_of_bins 80" + \
+               r" -f_one_half 10e-12" + \
+               r" -k_T 4.1e-21" + \
+               r" -velocity {}" + \
+               r" -flip_forces 0" + \
+               r" -file_input {}"+ \
+               r" -file_output landscape.csv {}").format(n_pairs,v,input_file,
+                                                         extra_str)
+    os.system(run_str)
+    input_data = np.loadtxt("landscape.csv",delimiter=",")
+    x_nm,G_kT = input_data[:,0]*1e9,input_data[:,1]/4.1e-21
+    return x_nm,G_kT
+
 def run():
     """
     <Description>
@@ -22,19 +37,23 @@ def run():
     Returns:
         This is a description of what is returned.
     """
-    os.system(r"python2 main_iwt.py -number_of_pairs 16" + \
-              r" -number_of_bins 80" + \
-              r" -f_one_half 10e-12" + \
-              r" -k_T 4.1e-21" + \
-              r" -velocity 20e-9" + \
-              r" -flip_forces 0" + \
-              r" -file_input ../Data/input.pxp"+ \
-              r" -file_output landscape.csv")
-    input_data = np.loadtxt("landscape.csv",delimiter=",")
-    x_nm,G_kT = input_data[:,0]*1e9,input_data[:,1]/4.1e-21
+    # run a comparison of fwd,rev,both
+    base = "../Data/"
+    input_files = [ [base + "UnfoldandRefold.pxp",""],
+                    ]
+    for f,extra_str in input_files:
+        # run just the 'normal' IO
+        x_nm,G_kT = run_single(n_pairs=50,v=50e-9,
+                               input_file=f,extra_str=extra_str)
+        plt.plot(x_nm,G_kT,'r-')
+        PlotUtilities.lazyLabel("x (nm)","$G_0$ ($k_\mathrm{B}$T)","")
+    plt.show()
+    # run just the 'normal' IO
+    x_nm,G_kT = run_single(n_pairs=16,v=20e-9,input_file="../Data/input.pxp")
     plt.plot(x_nm,G_kT,'r-')
     PlotUtilities.lazyLabel("x (nm)","$G_0$ ($k_\mathrm{B}$T)","")
     plt.show()
+     
 
 if __name__ == "__main__":
     run()
