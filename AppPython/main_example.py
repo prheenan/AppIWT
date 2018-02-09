e21f427c2025af9a046dfb3c964ae0893191d3da
Your Name <patirck.heenan@colorado.edu>
2018-02-09 15:15:53 -0700

    unfold and refolding are running... not sure why extensions are so much smaller with both?

--- AppPython/main_example.py
+++ AppPython/main_example.py
@@ -12,20 +12,24 @@ import sys,os
 sys.path.append("../")
 from UtilGeneral import PlotUtilities
 
-def run_single(n_pairs,v,input_file,extra_str=""):
+def run_single(n_pairs,v,input_file,f_one_half=10e-12,extra_str=""):
     run_str = (r"python2 main_iwt.py -number_of_pairs {}" + \
                r" -number_of_bins 80" + \
-               r" -f_one_half 10e-12" + \
+               r" -f_one_half {}" + \
                r" -k_T 4.1e-21" + \
                r" -velocity {}" + \
                r" -flip_forces 0" + \
                r" -file_input {}"+ \
-               r" -file_output landscape.csv {}").format(n_pairs,v,input_file,
+               r" -file_output landscape.csv {}").format(n_pairs,f_one_half,
+                                                         v,input_file,
                                                          extra_str)
     os.system(run_str)
     input_data = np.loadtxt("landscape.csv",delimiter=",")
-    x_nm,G_kT = input_data[:,0]*1e9,input_data[:,1]/4.1e-21
-    return x_nm,G_kT
+    kT = 4.1e-21
+    x_nm,G_kT,tilt_kT = input_data[:,0]*1e9,\
+                        input_data[:,1]/kT,\
+                        input_data[:,2]/kT
+    return x_nm,G_kT,tilt_kT
 
 def run():
     """
@@ -39,17 +43,24 @@ def run():
     """
     # run a comparison of fwd,rev,both
     base = "../Data/"
-    input_files = [ [base + "UnfoldandRefold.pxp",""],
+    plot_both = dict(color='r',linestyle='--',label="Bidirectional")
+    plot_unfold = dict(color='b',linestyle=':',label="Only Unfold")
+    plot_refold = dict(color='k',linestyle='-',label="Only Refold")
+    input_files = [ [base + "UnfoldandRefold.pxp","",plot_both],
+                    [base + "JustUnfold.pxp","-unfold_only 1",plot_unfold],
+                    [base + "JustRefold.pxp","-refold_only 1",plot_refold],
                     ]
-    for f,extra_str in input_files:
+    for f,extra_str,plot_opt in input_files:
         # run just the 'normal' IO
-        x_nm,G_kT = run_single(n_pairs=50,v=50e-9,
-                               input_file=f,extra_str=extra_str)
-        plt.plot(x_nm,G_kT,'r-')
-        PlotUtilities.lazyLabel("x (nm)","$G_0$ ($k_\mathrm{B}$T)","")
+        x_nm,G_kT,tilt = run_single(n_pairs=50,v=50e-9,f_one_half=13e-12,
+                                    input_file=f,extra_str=extra_str)
+        plt.plot(x_nm,G_kT,**plot_opt)
+        PlotUtilities.lazyLabel("x (nm)",
+                                "$\Delta G_\mathrm{0}$ ($k_\mathrm{B}$T)","")
     plt.show()
     # run just the 'normal' IO
-    x_nm,G_kT = run_single(n_pairs=16,v=20e-9,input_file="../Data/input.pxp")
+    x_nm,G_kT,tilt = \
+        run_single(n_pairs=16,v=20e-9,input_file="../Data/input.pxp")
     plt.plot(x_nm,G_kT,'r-')
     PlotUtilities.lazyLabel("x (nm)","$G_0$ ($k_\mathrm{B}$T)","")
     plt.show()
